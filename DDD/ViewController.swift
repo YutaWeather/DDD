21be2840d45f4d0f04d742b1322b945b50f564c4
Yuta Fujii <yutayutakid21@gmail.com>
2022-06-05 11:08:41 +0900

    AAA追加

--- DDD/ViewController.swift
+++ DDD/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("AAA")
         // Do any additional setup after loading the view.
     }
 
